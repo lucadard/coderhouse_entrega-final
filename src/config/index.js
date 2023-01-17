5baf1bbb6faf0b6b81e253753cbe3b63a37b9505
lucadard <lucadardenne@hotmail.com>
2023-01-17 13:10:13 -0300

    add variables to config file

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,31 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
+checkVars()
+function checkVars() {
+  const notSetVars = []
+  if (!process.env.JWT_SECRET) notSetVars.push('JWT_SECRET')
+  if (!process.env.EMAIL_SENDER_USER) notSetVars.push('EMAIL_SENDER_USER')
+  if (!process.env.EMAIL_SENDER_PASS) notSetVars.push('EMAIL_SENDER_PASS')
+  if (notSetVars.length > 0)
+    throw new Error(
+      'You need to set these environment variables: ' + notSetVars
+    )
+}
+
 export const config = {
-  admins: ['admin'],
-  mongoUrl: 'mongodb://localhost:27017/coder?authSource=admin',
+  admins: ['admin@admin.com'],
+  mongoUrl: 'mongodb://localhost:27017/coder',
+  staticPath: {
+    folder: 'static',
+    url: '/public',
+    defaultProfilePicture: '/public/images/no_profile_photo.jpeg',
+    defaultProductPicture: '/public/images/no_product_photo.jpeg'
+  },
   jwtSecret: process.env.JWT_SECRET,
-  staticPath: 'public/images',
-  environment: process.env.NODE_ENV
+  environment: process.env.NODE_ENV || 'development',
+  emailSender: {
+    user: process.env.EMAIL_SENDER_USER,
+    pass: process.env.EMAIL_SENDER_PASS
+  }
 }
