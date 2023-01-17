8ed91866deec4754ebc7ada4cafdff09a3b278be
lucadard <lucadardenne@hotmail.com>
2023-01-17 13:11:27 -0300

    implement send email and cost function

--- src/orders/OrdersService.js
+++ src/orders/OrdersService.js
@@ -2,7 +2,18 @@ import { randomUUID } from 'crypto'
 import { cartsService } from '../carts/index.js'
 import { CustomError } from '../models/CustomError.js'
 import { Order } from './models/Order.js'
+import { generateEmailText, sendOrderEmail } from '../config/nodemailer.js'
+import { usersService } from '../users/index.js'
 
+const addCostToProducts = (products) => {
+  console.log(products)
+  let productsWithCost = []
+  for (let product of products) {
+    const cost = product.cant * product.prod.price
+    productsWithCost.push({ ...product, cost })
+  }
+  return productsWithCost
+}
 export class OrdersService {
   #repository
   constructor(repository) {
@@ -13,15 +24,30 @@ export class OrdersService {
     return orders.map((order) => order.asDto())
   }
   createOrder = async (clientId) => {
-    const { products: cartProducts } = await cartsService.getCart(clientId)
-    if (cartProducts.length < 1)
+    const cartData = await cartsService.getCart(clientId)
+    if (cartData.products.length < 1)
       throw new CustomError('Cart is empty, could not create order.', 400)
     const createdOrder = await this.#repository.create(
-      new Order({ id: randomUUID(), clientId, prods: cartProducts })
+      new Order({
+        id: randomUUID(),
+        clientId,
+        prods: addCostToProducts(cartData.products)
+      })
     )
     if (!createdOrder) throw new CustomError('Could not create order', 500)
     const updatedCart = await cartsService.emptyCart(clientId)
-    // HERE SEND AN EMAIL TO ADMINS
+
+    const userData = await usersService.getUserById(clientId)
+
+    try {
+      const info = await sendOrderEmail(
+        generateEmailText(userData, createdOrder.asDto())
+      )
+      console.log('Email sent', info.messageId)
+    } catch (err) {
+      throw err
+    }
+
     return createdOrder.asDto()
   }
 }
