641924df8cc47a542e49c35f7329181228851bd1
lucadard <lucadardenne@hotmail.com>
2023-01-20 19:09:57 -0300

    add auto-incremental id

--- src/orders/schemas/orderSchema.js
+++ src/orders/schemas/orderSchema.js
@@ -1,10 +1,14 @@
-import { Schema } from 'mongoose'
+import mongoose, { Schema } from 'mongoose'
+import AutoIncrementFactory from 'mongoose-sequence'
+const AutoIncrement = AutoIncrementFactory(mongoose)
 
 export const OrderSchema = new Schema(
   {
-    id: { type: String, required: true, unique: true },
+    // id: { type: Number, required: true, unique: true },
     clientId: { type: String, required: true },
     prods: { _id: false, type: Array, default: [] }
   },
   { timestamps: true }
 )
+
+OrderSchema.plugin(AutoIncrement, { inc_field: 'id' })
