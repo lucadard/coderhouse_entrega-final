82b8473faba03475e5fae6e3ac518f6261c40db4
lucadard <lucadardenne@hotmail.com>
2023-01-20 19:10:31 -0300

    changes in schema

--- src/products/schemas/productSchema.js
+++ src/products/schemas/productSchema.js
@@ -2,9 +2,9 @@ import { Schema } from 'mongoose'
 
 export const ProductSchema = new Schema(
   {
-    id: { type: String, required: true, unique: true, },
+    id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
-    description: { type: String, required: true },
+    description: { type: String, default: '' },
     price: { type: Number, required: true },
     image: { type: String, required: true }
   },
