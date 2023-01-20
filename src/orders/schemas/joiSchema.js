b7d859dff8d718f083813b18fd9645d8e4d692ae
lucadard <lucadardenne@hotmail.com>
2023-01-20 19:09:45 -0300

    change id to not required for auto-increment

--- src/orders/schemas/joiSchema.js
+++ src/orders/schemas/joiSchema.js
@@ -1,7 +1,7 @@
 import Joi from 'joi'
 
 export const orderSchema = Joi.object({
-  id: Joi.string().required(),
+  id: Joi.number(),
   clientId: Joi.string().required(),
   prods: Joi.array().default([]),
   createdAt: Joi.date().default(Date.now)
