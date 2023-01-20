dc23625bf4558875b433455ba84da842d76b8125
lucadard <lucadardenne@hotmail.com>
2023-01-20 19:12:25 -0300

    change implementations

--- src/config/nodemailer.js
+++ src/config/nodemailer.js
@@ -11,18 +11,38 @@ const transporter = nodemailer.createTransport({
   }
 })
 
-export const sendOrderEmail = (text, html = '') =>
+export const sendEmail = async (to, subject, text, html) => {
   transporter.sendMail({
-    from: '"Coder App 💻" <app@coder.com>', // sender address
-    to: config.admins.reduce((acc, cur) => (acc ? acc + ',' + cur : cur), ''), // list of receivers
-    subject: 'Nueva orden ✔', // Subject line
+    from: '"Coder App 💻" <app@coder.com>',
+    to:
+      to === 'admin' // generates string to include all admins
+        ? config.admins.reduce((acc, cur) => (acc ? acc + ',' + cur : cur), '')
+        : to,
+    subject,
     text,
     html
   })
+}
 
-export const generateEmailText = (userData, orderData) => {
+export const generateAdminEmail = (userData, orderData) => {
   return `Recibiste una orden:\nId de la orden: ${orderData.id}\nUsuario: ${
     userData.email
+  }\nFecha: ${new Date(
+    orderData.createdAt
+  ).toLocaleString()}\nProductos:\n${orderData.prods.reduce(
+    (acc, cur, i) =>
+      `${acc ? acc + '\n' : acc}-------------------------\n#${i + 1}\nId: ${
+        cur.prod.id
+      }\nNombre: ${cur.prod.name}\nCantidad: ${cur.cant}\nPrecio por unidad: $${
+        cur.prod.price
+      }\nPrecio total: $${cur.cost}`,
+    ``
+  )}`
+}
+
+export const generateUserEmail = (orderData) => {
+  return `Recibimos tu orden!\nId de la orden: ${
+    orderData.id
   }\nFecha: ${new Date(
     orderData.createdAt
   ).toLocaleString()}\nProductos:\n${orderData.prods.reduce(
