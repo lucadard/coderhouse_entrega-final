4c0e2581d05c8e1bb12fad037f2602fc0153446d
lucadard <lucadardenne@hotmail.com>
2023-01-20 19:10:13 -0300

    change requirements to schema

--- src/products/schemas/joiSchema.js
+++ src/products/schemas/joiSchema.js
@@ -3,16 +3,16 @@ import { config } from '../../config/index.js'
 
 export const productSchema = Joi.object({
   id: Joi.string().required(),
-  name: Joi.string().min(3).max(50).required(),
-  description: Joi.string().min(3).max(120).default(''),
+  name: Joi.string().min(3).max(150).required(),
+  description: Joi.string().max(500).empty('').default(''),
   price: Joi.number().min(1).required(),
   image: Joi.string().default(config.staticPath.defaultProfilePicture)
 })
 
 export const updateSchema = Joi.object({
   id: Joi.string().forbidden(),
-  name: Joi.string().min(3).max(50),
-  description: Joi.string().min(3).max(120),
+  name: Joi.string().min(3).max(150),
+  description: Joi.string().max(500).empty('').default(''),
   price: Joi.number().min(1),
   image: Joi.string()
 })
