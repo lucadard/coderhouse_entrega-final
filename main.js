f488baa7501d5a6e18a161680b52e34621cf56b4
lucadard <lucadardenne@hotmail.com>
2023-01-21 17:16:23 -0300

    change console.log for logger and add port env var

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 import { createServer } from './src/server.js'
 import { networkInterfaces } from 'os'
+import { logger } from './src/config/logger.js'
+import { vars } from './src/config/vars.js'
 const [IP] = networkInterfaces()['en0'].filter((net) => net.family === 'IPv4')
 
 try {
-  const server = await createServer(8080)
-  console.log(
+  const server = await createServer(vars.port || 8080)
+  logger.info(
     `Server listening on: http://${IP.address}:${server.address().port}`
   )
 } catch (err) {
-  console.log(err)
+  logger.error(err)
 }
