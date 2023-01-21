0e0ab122c7c4016d40b32c478271a3bc9c08f3c9
lucadard <lucadardenne@hotmail.com>
2023-01-21 17:54:32 -0300

    remove log

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
 import { createServer } from './src/server.js'
-import { networkInterfaces } from 'os'
 import { logger } from './src/config/logger.js'
 import { vars } from './src/config/vars.js'
-const [IP] = networkInterfaces()['en0'].filter((net) => net.family === 'IPv4')
 
 try {
   const server = await createServer(vars.port || 8080)
-  logger.info(
-    `Server listening on: http://${IP.address}:${server.address().port}`
-  )
+  logger.info(`Server listening on port ${server.address().port}`)
 } catch (err) {
   logger.error(err)
 }
