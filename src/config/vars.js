c240041548f7df005d908cb58a43401fb5597e50
lucadard <lucadardenne@hotmail.com>
2023-01-21 17:18:29 -0300

    add port var, node_env logger

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,10 +1,17 @@
 import dotenv from 'dotenv'
+import { logger } from './logger.js'
 dotenv.config()
+
+logger.info('Environment: ' + process.env.NODE_ENV)
 
 checkVars()
 function checkVars() {
   const notSetVars = []
-  if (!process.env.MONGO_URL) notSetVars.push('MONGO_URL')
+  if (process.env.NODE_ENV === 'production')
+    if (!process.env.MONGO_PROD_URL) notSetVars.push('MONGO_PROD_URL')
+    else {
+      if (!process.env.MONGO_DEV_URL) notSetVars.push('MONGO_DEV_URL')
+    }
   if (!process.env.JWT_SECRET) notSetVars.push('JWT_SECRET')
   if (!process.env.EMAIL_SENDER_USER) notSetVars.push('EMAIL_SENDER_USER')
   if (!process.env.EMAIL_SENDER_PASS) notSetVars.push('EMAIL_SENDER_PASS')
@@ -22,9 +29,13 @@ export const vars = {
     defaultProfilePicture: '/public/defaults/no-pfp.jpeg',
     defaultProductPicture: '/public/defaults/no-prod.jpeg'
   },
-  mongoUrl: process.env.MONGO_URL,
-  jwtSecret: process.env.JWT_SECRET,
   environment: process.env.NODE_ENV,
+  port: process.env.PORT,
+  jwtSecret: process.env.JWT_SECRET,
+  mongoUrl:
+    process.env.NODE_ENV === 'production'
+      ? process.env.MONGO_PROD_URL
+      : process.env.MONGO_DEV_URL,
   emailConfig: {
     user: process.env.EMAIL_SENDER_USER,
     pass: process.env.EMAIL_SENDER_PASS
