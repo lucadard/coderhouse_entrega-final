269ce551e3de2c9a53562410af77b791805809f6
lucadard <lucadardenne@hotmail.com>
2023-01-21 18:47:33 -0300

    change code order

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,29 +1,9 @@
 import dotenv from 'dotenv'
 import { logger } from './logger.js'
 dotenv.config()
-
-logger.info(
-  `Environment ${
-    process.env.NODE_ENV !== 'development' ? 'production' : 'development'
-  }`
-)
-
 checkVars()
-function checkVars() {
-  const notSetVars = []
-  if (process.env.NODE_ENV !== 'development')
-    if (!process.env.MONGO_PROD_URL) notSetVars.push('MONGO_PROD_URL')
-    else {
-      if (!process.env.MONGO_DEV_URL) notSetVars.push('MONGO_DEV_URL')
-    }
-  if (!process.env.JWT_SECRET) notSetVars.push('JWT_SECRET')
-  if (!process.env.EMAIL_SENDER_USER) notSetVars.push('EMAIL_SENDER_USER')
-  if (!process.env.EMAIL_SENDER_PASS) notSetVars.push('EMAIL_SENDER_PASS')
-  if (notSetVars.length > 0)
-    throw new Error(
-      'You need to set these environment variables: ' + notSetVars
-    )
-}
+
+logger.info(`Environment ${process.env.NODE_ENV || 'production'}`)
 
 export const vars = {
   admins: ['admin@admin.com'],
@@ -44,4 +24,20 @@ export const vars = {
     user: process.env.EMAIL_SENDER_USER,
     pass: process.env.EMAIL_SENDER_PASS
   }
+}
+
+function checkVars() {
+  const notSetVars = []
+  if (process.env.NODE_ENV !== 'development')
+    if (!process.env.MONGO_PROD_URL) notSetVars.push('MONGO_PROD_URL')
+    else {
+      if (!process.env.MONGO_DEV_URL) notSetVars.push('MONGO_DEV_URL')
+    }
+  if (!process.env.JWT_SECRET) notSetVars.push('JWT_SECRET')
+  if (!process.env.EMAIL_SENDER_USER) notSetVars.push('EMAIL_SENDER_USER')
+  if (!process.env.EMAIL_SENDER_PASS) notSetVars.push('EMAIL_SENDER_PASS')
+  if (notSetVars.length > 0)
+    throw new Error(
+      'You need to set these environment variables: ' + notSetVars
+    )
 }
